b5fccbda96ba2d82b46d08ecbabd94f0a438cfc4
kmjnn <ZhavoronkovaNA444@yandex.ru>
2026-02-23 01:20:59 +0300

    backend: добавление масок + окно редактирования
    добавлены маски для ввода паспорта и номера телефона. добавлено окно редактирования

--- web/script.js
+++ web/script.js
@@ -36,6 +36,42 @@ function renderTable(){
     })
 }
 
+function applyMasks(){
+    const passportInput = document.getElementById('passport');
+    passportInput.addEventListener('input', (e) => {
+        let x = e.target.value.replace(/\D/g, '').match(/(\d{0,4})(\d{0,6})/);
+        e.target.value = !x[2] ? x[1] : x[1] + ' ' + x[2];
+    })
+    const contactsInput = document.getElementById('contacts');
+    contactsInput.value = '';
+    contactsInput.addEventListener('input', (e) => { 
+        let value = e.target.value.replace(/\D/g, '');
+        let formattedValue = '';
+    
+        if (value.length > 0) {
+        formattedValue = '+7 (';
+        if (value.length > 0) formattedValue += value.slice(0, 3);
+        if (value.length > 3) formattedValue += ') ' + value.slice(3, 6);
+        if (value.length > 6) formattedValue += '-' + value.slice(6, 8);
+        if (value.length > 8) formattedValue += '-' + value.slice(8, 10);
+        }
+        
+        e.target.value = formattedValue;
+        e.target.setSelectionRange(formattedValue.length, formattedValue.length);
+    });
+}
+
+async function loadEmployees(dept = '', pos = '') {
+  try {
+    const url = `/api/employees?department=${dept}&position=${pos}`;
+    const res = await fetch(url);
+    employees = await res.json();
+    renderTable();
+  } catch (err) {
+    alert('Ошибка загрузки: ' + err.message);
+  }
+}
+
 async function searchEmployee(params) {
     const name = document.getElementById('searchName').ariaValueMax;
     if (!name) return loadEmployees();
@@ -47,4 +83,21 @@ async function searchEmployee(params) {
         alert('Ошибка поиска: '+ err.message)
     }
     
+}
+
+function openModal(id = null) {
+  editingId = id;
+  document.getElementById('modalTitle').textContent = id ? 'Редактировать' : 'Новый сотрудник';
+  if (id) {
+    fetch(`/api/employees/${id}`).then(res => res.json()).then(emp => {
+      Object.keys(emp).forEach(key => {
+        const input = document.getElementById(key);
+        if (input) input.value = emp[key] || '';
+      });
+    });
+  } else {
+    document.getElementById('employeeForm').reset();
+  }
+  document.getElementById('modal').style.display = 'block';
+  applyMasks();
 }
